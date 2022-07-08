598519187f893577f3a01344b9d9e6fe661aef3b
Mr. Horbio <89149725+Mr-Horbio@users.noreply.github.com>
2022-07-08 16:20:51 -0700

    Add files via upload

---
+++ table.py
@@ -0,0 +1,10 @@
+# # we write multiplication table in python. in while loop
+ 
+no=int(input("Enter the no:"))
+
+count=1
+while count<=10:
+    product=no*count
+    print(no, ' x' ,count, ' =' , product)
+    count=count+1
+
